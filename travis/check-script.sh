4565df1c0b8a31905284d90954c864eee1515796
Neil Wu <neilwu@umich.edu>
2020-09-29 20:32:54 -0400

    Create check-script.sh

---
+++ travis/check-script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -ev
+
+if [[ "$TEST_TYPE" == "check" ]]; then
+    if [[ -f ".flake8" ]]; then
+        wget https://raw.githubusercontent.com/mdolab/.github/master/.flake8 -O .flake8.temp;
+        cat .flake8.temp .flake8 > .flake8
+    else
+        wget https://raw.githubusercontent.com/mdolab/.github/master/.flake8;
+    fi
+    flake8 . --count --show-source --statistics;
+    black . --check -l 120 --target-version py$PY;
+fi

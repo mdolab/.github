12de504f8c2ced746a1cd2f89c55e7bc62f9d615
Neil Wu <neilwu@umich.edu>
2020-09-30 11:48:31 -0400

    added separate black and flake8 jobs

--- travis/check-script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# don't download the reference flake8 config if one already exists in the repo
-if [[ ! -f ".flake8" ]]; then
-    wget https://raw.githubusercontent.com/mdolab/.github/master/.flake8;
-fi
-
-flake8 . --count --show-source --statistics;
-black . --check -l 120 --target-version py$PY;

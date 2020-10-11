ad7995fcfae0cdb89e750c2ece36afd3b807f478
Neil Wu <neilwu@umich.edu>
2020-10-11 19:58:09 -0400

    added conditional complex builds

--- travis/install-package.sh
+++ travis/install-package.sh
@@ -3,11 +3,10 @@ set -ev
 
 # Copy over the correct config file and modify as needed
 docker exec -it app /bin/bash -c ". $BASHRC && cd $DOCKER_WORKING_DIR && cp $CONFIG_MK config/config.mk";
-# compile real build
-docker exec -it app /bin/bash -c ". $BASHRC && cd $DOCKER_WORKING_DIR && make"
-# compile complex build
-# check that the complex make file exists (in the Travis VM, no need to check in Docker)
-if [ -f "Makefile_CS" ]; then
+if [[ "$TEST_COMPLEX" != "true" ]]; then # compile real build
+    docker exec -it app /bin/bash -c ". $BASHRC && cd $DOCKER_WORKING_DIR && make"
+elif [[ "$TEST_COMPLEX" == "true" ]] && [ -f "Makefile_CS" ]; then # compile complex build iff complex makefile exists
+    # we only check the makefile in the Travis VM, no need to check in Docker
     docker exec -it app /bin/bash -c ". $BASHRC && cd $DOCKER_WORKING_DIR && make -f Makefile_CS PETSC_ARCH=complex-opt-\$COMPILERS-\$PETSCVERSION";
 fi
 # Install Python interface

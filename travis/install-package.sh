6ccac0f9ebad0d00f5cbb163346addf985e2e193
Neil Wu <neilwu@umich.edu>
2020-10-12 14:12:46 -0400

    Update install-package.sh

--- travis/install-package.sh
+++ travis/install-package.sh
@@ -3,12 +3,17 @@ set -ev
 
 # Copy over the correct config file and modify as needed
 docker exec -it app /bin/bash -c ". $BASHRC && cd $DOCKER_WORKING_DIR && cp $CONFIG_MK config/config.mk";
-if [[ "$DOCKER_TEST_TYPE" == "real" ]]; then # compile real build
+
+# compile real build
+if [[ "$DOCKER_TEST_TYPE" == "real" ]]; then
     docker exec -it app /bin/bash -c ". $BASHRC && cd $DOCKER_WORKING_DIR && make"
-    # compile complex build iff complex makefile exists, and either TEST_TYPE=complex OR not separate build
+fi
+
+# compile complex build iff complex makefile exists, and either TEST_TYPE=complex OR not separate build
+# we only check the makefile in the Travis VM, no need to check in Docker
 if [[ "$SEPARATE_COMPLEX_BUILD" != true || "$DOCKER_TEST_TYPE" == "complex" ]] && [ -f "Makefile_CS" ]; then
-    # we only check the makefile in the Travis VM, no need to check in Docker
     docker exec -it app /bin/bash -c ". $BASHRC && cd $DOCKER_WORKING_DIR && make -f Makefile_CS PETSC_ARCH=complex-opt-\$COMPILERS-\$PETSCVERSION";
 fi
+
 # Install Python interface
 docker exec -it app /bin/bash -c ". $BASHRC && cd $DOCKER_WORKING_DIR && pip install ."

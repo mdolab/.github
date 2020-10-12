9fffe3bd67434cf49cd4f3ef44f1f3b926505069
Neil Wu <neilwu@umich.edu>
2020-10-12 13:04:41 -0400

    Update install-package.sh

--- travis/install-package.sh
+++ travis/install-package.sh
@@ -5,7 +5,8 @@ set -ev
 docker exec -it app /bin/bash -c ". $BASHRC && cd $DOCKER_WORKING_DIR && cp $CONFIG_MK config/config.mk";
 if [[ "$DOCKER_TEST_TYPE" == "real" ]]; then # compile real build
     docker exec -it app /bin/bash -c ". $BASHRC && cd $DOCKER_WORKING_DIR && make"
-elif [[ "$DOCKER_TEST_TYPE" == "complex" ]] && [ -f "Makefile_CS" ]; then # compile complex build iff complex makefile exists
+# compile complex build iff complex makefile exists, and either TEST_TYPE=complex OR not separate build
+elif [[ "$SEPARATE_COMPLEX_BUILD" != true || "$DOCKER_TEST_TYPE" == "complex" ]] && [ -f "Makefile_CS" ]; then
     # we only check the makefile in the Travis VM, no need to check in Docker
     docker exec -it app /bin/bash -c ". $BASHRC && cd $DOCKER_WORKING_DIR && make -f Makefile_CS PETSC_ARCH=complex-opt-\$COMPILERS-\$PETSCVERSION";
 fi

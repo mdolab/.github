cb8419b5cf758250d94682b8325317a671fa657d
Neil Wu <602725+nwu63@users.noreply.github.com>
2021-08-16 14:02:45 -0400

    added line breaks to the shell script

--- generate-pylintrc.sh
+++ generate-pylintrc.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/sh
 
-pylint --disable all --enable basic,classes,exceptions,miscellaneous,newstyle,refactoring,similarities,stdlib,string,typecheck,variables --disable c-extension-no-member,anomalous-backslash-in-string,missing-module-docstring,missing-class-docstring,missing-function-docstring,fixme --argument-naming-style=any --argument-naming-style=any --attr-naming-style=any --function-naming-style=any --method-naming-style=any --module-naming-style=any --variable-naming-style=any --class-naming-style=any --generate-rcfile > .pylintrc
+pylint \
+--disable all \
+--enable basic,classes,exceptions,miscellaneous,newstyle,refactoring,similarities,stdlib,string,typecheck,variables \
+--disable c-extension-no-member,anomalous-backslash-in-string,missing-module-docstring,missing-class-docstring,missing-function-docstring,fixme \
+--argument-naming-style=any \
+--argument-naming-style=any \
+--attr-naming-style=any \
+--function-naming-style=any \
+--method-naming-style=any \
+--module-naming-style=any \
+--variable-naming-style=any \
+--class-naming-style=any \
+--generate-rcfile \
+> .pylintrc

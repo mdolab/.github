2fade4a1d8b560e66ebc59259a1d7b3d9ba2b11c
Alasdair Gray <alachris@umich.edu>
2021-08-31 14:22:00 -0400

    Update buildbot_setup.sh (#23)

--- build-bot/buildbot_setup.sh
+++ build-bot/buildbot_setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 ### Reading the input token for docker install
 if [ "$1" != "" ]; then
     TOKEN=$1
@@ -9,10 +11,16 @@ else
 fi
 
 ### Base packages installation
+echo "Installing base packages"
 sudo apt-get update && sudo apt-get -y upgrade
-sudo apt-get install openssh-server vim cmake build-essential
+sudo apt-get -y install openssh-server vim cmake build-essential
+echo "Finished installing APT packages"
+echo ""
+echo ""
+echo ""
 
 ### Install Docker
+echo "Installing docker"
 sudo apt-get install -yq \
     apt-transport-https \
     ca-certificates \
@@ -24,11 +32,15 @@ echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 # add docker to the sudo group
 sudo groupadd docker -f
 sudo usermod -aG docker $USER
 newgrp docker
+echo ""
+echo ""
+echo ""
+echo "Testing that docker works"
 # test that docker works
 docker run hello-world
 if [ $? -ne 0 ]; then
@@ -37,10 +49,22 @@ if [ $? -ne 0 ]; then
 fi
 
 # enable the docker service
+echo ""
+echo ""
+echo ""
+echo "Enabling docker service"
 sudo systemctl enable docker.service
 sudo systemctl enable containerd.service
+echo ""
+echo ""
+echo ""
+echo "Docker service enabled"
 
 # Create a folder
+echo ""
+echo ""
+echo ""
+echo "setting up github actions runner"
 mkdir $HOME/actions-runner && cd $HOME/actions-runner
 # Download the latest runner package
 curl -o actions-runner-linux-x64-2.279.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.279.0/actions-runner-linux-x64-2.279.0.tar.gz
@@ -52,6 +76,10 @@ if [ $? -ne 0 ]; then
 fi
 
 # Extract the installer
+echo ""
+echo ""
+echo ""
+echo "Installing github actions runner"
 tar xzf ./actions-runner-linux-x64-2.279.0.tar.gz
 # Create the runner and start the configuration experience
 ./config.sh --url https://github.com/mdolab --token $TOKEN --unattended
@@ -59,3 +87,7 @@ tar xzf ./actions-runner-linux-x64-2.279.0.tar.gz
 sudo ./svc.sh install
 # Start the service
 sudo ./svc.sh start
+echo ""
+echo ""
+echo ""
+echo "SETUP COMPLETE"

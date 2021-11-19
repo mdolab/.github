76db0a614023fb0b3f7fd4e6296d63f79a592be2
Neil Wu <neilwu0626@gmail.com>
2021-11-18 23:58:00 -0500

    updated pylint config

--- generate-pylintrc.sh
+++ generate-pylintrc.sh
@@ -2,15 +2,7 @@
 
 pylint \
 --disable all \
---enable basic,classes,exceptions,miscellaneous,newstyle,refactoring,similarities,stdlib,string,typecheck,variables \
---disable c-extension-no-member,anomalous-backslash-in-string,missing-module-docstring,missing-class-docstring,missing-function-docstring,fixme \
---argument-naming-style=any \
---argument-naming-style=any \
---attr-naming-style=any \
---function-naming-style=any \
---method-naming-style=any \
---module-naming-style=any \
---variable-naming-style=any \
---class-naming-style=any \
+--enable basic,classes,exceptions,newstyle,refactoring,stdlib,string,typecheck,variables \
+--disable C,R,I,unspecified-encoding,protected-access \
 --generate-rcfile \
 > .pylintrc

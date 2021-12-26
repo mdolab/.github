3d3b8235d562093d02fbe7a5f9bad78e3d7fa618
Neil Wu <602725+nwu63@users.noreply.github.com>
2021-12-25 22:07:31 -0500

    added import-error to list of ignored errors, updated rc file

--- generate-pylintrc.sh
+++ generate-pylintrc.sh
@@ -3,6 +3,6 @@
 pylint \
 --disable all \
 --enable basic,classes,exceptions,imports,newstyle,refactoring,stdlib,string,typecheck,variables \
---disable C,R,I,unspecified-encoding,protected-access \
+--disable C,R,I,unspecified-encoding,protected-access,import-error \
 --generate-rcfile \
 > .pylintrc
